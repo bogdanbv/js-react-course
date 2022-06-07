12124f8249587b486b324fd17b75dad2739a1311
bogdanbv <bogdanweb3@gmail.com>
2022-06-07 23:35:44 +0200

    Greate new project in new lessons

---
+++ lesson1/help.js
@@ -0,0 +1,29 @@
+"use strict";
+ #ОПЕРАТОРЫ:
+= // присвоениие
+== // равенство значений
+=== // строгое равенство
+% // остаток после деления первого числа на второе
+&& // и
+|| // или
+! // отрицание !true или не равен !=
+// ТАБЛИЦА ПРИОРИТЕТОВ ОПЕРАТОРОВhttps://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+
+--somevar // инкримент в префиксе
+somevar-- // инкримент в постфиксным
+somevar--
+let name; // для изменяемых
+const nameConstant; // для не изменяемых
+const arr = [1, 2, "john"]; // массив, перечень инфо по порядку
+answers[3] = prompt('What is your name?', ''); // спросить данные у пользователя и записать их в массив
+console.log(arr[1]); // показать второе значение массива
+const obj {name: John, age: 25,} // обьект, содержит ключ и данные
+console.log (obj.name); console.log(obj[name]); // показать значение name массива
+obj.b = '1234'; obj['b'] = '1234'; // добавить значение к обьекту (если во втором примере указать без кавычек то обратимся к переменной)
+
+alert("Alert"); // всплывающее окно с тектсом
+const result = confirm("Are you human?"); // попап спросить возраст
+const answer = prompt ("How old are you?" "18"); // попап спросить возраст и дать возможность написать знаение, тип данных строка, чтобы полуить номер, поставить + перед prompt
+const category ='toys'; console.log(`https://mysite.com/${category}`); //вставить переменную через специальыне скобочки с тильды
+
+
---
+++ lesson1/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// alert("hello");
+
+// const answer = prompt("How old are you?", "18");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('Date of birthd?', '');
+answers[2] = prompt('How old are you?', '');
+
+//document.write(answers);
+document.write(typeof(answers));
