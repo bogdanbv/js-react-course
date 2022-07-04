c929e8cd855e627a57a9e490229b1460678d6b1b
bogdanbv <bogdanweb3@gmail.com>
2022-07-04 16:27:28 +0200

    AJAX and XMLHttpRequest in help.js file

--- help.js
+++ help.js
@@ -465,6 +465,31 @@ clone.parents.mom = 'Ann';
 console.log(persone);
 console.log(clone);
 
+
+
+#AJAX и Cервер
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest(); 
+    request.open('GET', 'js/current.json'); // собирает настройки которые в будущем помогут сделать запрос, вид запроса и путь
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', () => { // реагирует на изменение статуса запроса, чаще используют просто load
+        if (request.readyState === 4 && request.status === 200) { // если статус запроса = 4 (done), и ответ сервера получен ОК (200), с условием load используют только request.status === 200
+            console.log(request.response); // вывести ответ в консоль
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2); //делем знаение рубля на курс usd и выводим с округлением до 2х символов после точки
+        } else {
+            inputUsd.value = 'Some error';
+        }
+    });
+});
+
+
+
+
 #МЕТОДЫ И СВОЙСТВА ЧИСЕЛ И строк
 console.dir(Number); // просмотреть все методы и свойства чисел в консоли
 console.log(Math.round(1.4)); // округлить
