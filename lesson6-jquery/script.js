ad4a07cb464b10dd7f6ea8e12bf7c19a86ccd7fc
bogdanbv <bogdanweb3@gmail.com>
2022-07-16 17:30:06 +0200

    Some practice with JQuery

--- lesson6-jquery/script.js
+++ lesson6-jquery/script.js
@@ -1,1 +1,19 @@
-4
+import $ from 'jquery';
+
+$(document).ready(function() {
+ $('.list-item:first').hover(function() {
+    $(this).toggleClass('active');
+ });
+
+ $('.list-item:eq(2)').on('click', function() {
+    $('.image:even').fadeToggle('slow');
+ });
+
+ $('.list-item:eq(3)').on('click', function() {
+    $('.image:odd').animate({
+        opacity: 'toggle',
+        height: 'toggle'
+    }, 2000);
+ });
+
+});
